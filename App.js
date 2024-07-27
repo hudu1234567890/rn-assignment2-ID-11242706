95340c774a7d68ac0c44bbdda77455d6831df13a
hudutrip <126909852+hudutrip@users.noreply.github.com>
2024-07-27 14:52:49 +0000

    Suleman Hudu DCIT 202 Assignment 2

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subContainer}>My name is <Text style={styles.format}>Suleman Hudu</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  format: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subContainer: {
+    fontSize: 24,
+  }
+});
---
+++ README.md
@@ -0,0 +1,14 @@
+# React Native Assignment 
+
+**Student ID:** [11242706]
+
+## Description
+
+This is a React Native project created using Expo CLI. The application has the following features:
+
+- The background color of the `View` component is changed to ``.
+- The `Text` component displays "My name is **Suleman Hudu**", where "YourName" is displayed in bold.
+- The font size of the text is increased to 24.
+
+## Screenshot
+![alt text](<assets/WhatsApp Image 2024-07-27 at 13.54.38_7c704dc0.jpg>)
